9fe3be5b9b2a1cfc21cbba6140f43c62e8b26a93
xue <xuefenglei04@126.com>
2017-11-24 14:58:06 +0800

    add tests

--- tests/init.sh
+++ tests/init.sh
@@ -8,7 +8,7 @@ curl -XPUT      'http://localhost:9200/equery?pretty'  -H 'Content-Type: applica
                 "field1": {"type": "text"       },
                 "field2": {"type": "keyword"    },
                 "field3": {"type": "integer"    },
-                "field4": {"type": "date",      "doc_values": true,     "format": "yyyy-MM-dd HH:mm:ss||yyyy-MM-dd||epoch_millis|yyyy-MM-ddTHH:mm:ssZ"}
+                "field4": {"type": "date",      "doc_values": true,     "format": "yyyy-MM-dd HH:mm:ss||epoch_millis||date_hour_minute_second||date_time_no_millis"}
             }
         }
     }
@@ -34,6 +34,6 @@ curl -XPUT 'http://localhost:9200/equery/equery/3?pretty'  -H 'Content-Type: app
                 "field1": "or not to be",
                 "field2": "to be2",
                 "field3": 8,
-                "field4": "2014-09-12 10:10:10"
+                "field4": "2014-09-13 10:10:10"
 }'
 
--- tests/requests.php
+++ tests/requests.php
@@ -5,9 +5,85 @@ use PHPUnit\Framework\TestCase;
 use EQuery\dsl;
 use EQuery\dsl\text;
 
+
 class TestRequest extends TestCase {
-    public function testPushAndPop() {
-        $stack = [];
-        $this->assertEquals(0, count($stack));
+    public static function setUpBeforeClass()
+    {
+        // do init
+        exec("sh ". __DIR__. DIRECTORY_SEPARATOR. "init.sh");
+    }
+
+    public function testrequest0() {
+        $rb = new EQuery\request\requestbody();
+        $dsl = _bool()->must_not(_in("field3", [8, 29]));
+        $rb->Query($dsl)->From(0)->Size(10)->Sort(["field4"=>"desc"]);
+        $rq = new EQuery\request\request(["host"=>"localhost", "port"=>"9200", "path" => "/equery/_search"]);
+        $opts = ["header" => array(), "timeout" => 5];
+        $a = $rq->doRequest($rb, $opts);
+        $result = json_decode($a["result"], 1);
+        $expect = Array (
+                    "took" => 7,
+                    "timed_out" => false,
+                    "_shards" => Array
+                        (
+                            "total" => 5,
+                            "successful" => 5,
+                            "skipped" => 0,
+                            "failed" => 0
+                        ),
+                    "hits" => Array(
+                            "total" => 1,
+                            "max_score" => "",
+                            "hits" => Array(
+                                    Array
+                                        (
+                                            "_index" => "equery",
+                                            "_type" => "equery",
+                                            "_id" => 1,
+                                            "_score" => "",
+                                            "_source" => Array(
+                                                    "field1" => "to be or not to be",
+                                                    "field2" => "to be",
+                                                    "field3" => "9",
+                                                    "field4" => "2014-09-11 10:10:10"
+                                                ),
+                                            "sort" => Array(
+                                                    1410430210000
+                                                )
+                                        )
+                                )
+                        )
+                );
+        $this->assertEquals($expect["hits"], $result["hits"]);
+    }
+
+    public function testrequest1() {
+        $rb = new EQuery\request\requestbody();
+        $dsl = _and(_and(_gteq("field4", "2012-10-11 00:00:00"), _lt("field4", "2017-11-11T00:00:00Z")), _or(_not(_eq("field2", "testnot")), _match("field1", "to be")), _in("field3", [8,9, 10]));
+        $rb->Query($dsl)->From(0)->Size(10);
+        $rq = new EQuery\request\request(["host"=>"localhost", "port"=>"9200", "path" => "/equery/_search"]);
+        $opts = ["header" => array(), "timeout" => 5];
+        $a = $rq->doRequest($rb, $opts);
+        $result = json_decode($a["result"], 1);
+        $hits = [
+            "total" => 3,
+            "max_score" => 4.791126,
+            "hits" =>[
+                    ["_index"=>"equery","_type"=>"equery","_id"=>"1","_score"=>4.791126,"_source"=>[
+                        "field1"=> "to be or not to be",
+                        "field2"=> "to be",
+                        "field3"=> 9,
+                        "field4"=> "2014-09-11 10:10:10"
+                    ]],["_index"=>"equery","_type"=>"equery","_id"=>"3","_score"=>4.575364,"_source"=>[
+                        "field1"=> "or not to be",
+                        "field2"=> "to be2",
+                        "field3"=> 8,
+                        "field4"=> "2014-09-13 10:10:10"
+                    ]],["_index"=>"equery","_type"=>"equery","_id"=>"2","_score"=>4.0,"_source"=>[
+                        "field2"=> "to be2",
+                        "field3"=> 8,
+                        "field4"=> "2014-09-12 10:10:10"
+                    ]]]];
+        $this->assertEquals($hits, $result["hits"]);
     }
 }
